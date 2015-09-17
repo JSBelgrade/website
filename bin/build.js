23c7d53fe3aae9c9e9add09576d06a9b5ad95c04
Slobodan Stojanovic <slobodan@cloudhorizon.com>
2015-09-17 11:55:16 +0200

    refactor(standards): add standards and update the code
    This commit is adding JS standards and editor config and refactoring scripts from bin folder and siteInfo.js to match those standards. It's also adding a check for standards when you run 'npm start' and badge to readme. This is not a best way to check if standards are ok, but it's a beginning.

--- bin/build.js
+++ bin/build.js
@@ -1,78 +1,84 @@
 #!/usr/bin/env node
 
-'use strict';
-const path = require('path');
-const Metalsmith = require('metalsmith');
-const layouts = require('metalsmith-layouts');
-const markdown = require('metalsmith-markdown');
-const permalinks = require('metalsmith-permalinks');
-const metalsmithPrism = require('metalsmith-prism');
-const collections = require('metalsmith-collections');
-const feed = require('metalsmith-feed');
-const mapHandlebarsPartials = require('../scripts/mapHandlebarsPartials');
-const siteInfo = require('../siteInfo');
-const ncp = require('ncp');
-const dir = path.join(__dirname, '..');
+'use strict'
 
-function build(done) {
+const path = require('path')
+const Metalsmith = require('metalsmith')
+const layouts = require('metalsmith-layouts')
+const markdown = require('metalsmith-markdown')
+const permalinks = require('metalsmith-permalinks')
+const metalsmithPrism = require('metalsmith-prism')
+const collections = require('metalsmith-collections')
+const feed = require('metalsmith-feed')
+const mapHandlebarsPartials = require('../scripts/mapHandlebarsPartials')
+const siteInfo = require('../siteInfo')
+const ncp = require('ncp')
+const dir = path.join(__dirname, '..')
 
-    const metalsmith = Metalsmith(dir);
+function build (done) {
+  const metalsmith = Metalsmith(dir)
 
-    metalsmith.metadata({site: siteInfo});
+  metalsmith.metadata({
+    site: siteInfo
+  })
 
-     metalsmith
-        .source(path.join(dir, 'content'))
-        .use(collections({
-            blog : {
-                pattern: 'blog/**/*.md',
-                sortBy: 'date',
-                reverse: true,
-                refer: false
-            },
-        }))
-        .use(feed({
-            collection: 'blog'
-        }))
-        .use(markdown({
-            langPrefix: 'language-'
-        }))
-        .use(metalsmithPrism())
-        .use(permalinks({
-            relative: false
-        }))
-        .use(layouts({
-            engine: 'handlebars',
-            pattern: '**/*.html',
-            partials: mapHandlebarsPartials(metalsmith, 'layouts', 'partials'),
-            helpers: {
-                excerpt: require('../scripts/helpers/excerpt'),
-                moment: require('../scripts/helpers/moment'),
-            }
-        }))
-        .destination(path.join(dir, 'dist'));
+  metalsmith
+    .source(path.join(dir, 'content'))
+      .use(collections({
+        blog: {
+          pattern: 'blog/**/*.md',
+          sortBy: 'date',
+          reverse: true,
+          refer: false
+        }
+      }))
+      .use(feed({
+        collection: 'blog'
+      }))
+      .use(markdown({
+        langPrefix: 'language-'
+      }))
+      .use(metalsmithPrism())
+      .use(permalinks({
+        relative: false
+      }))
+      .use(layouts({
+        engine: 'handlebars',
+        pattern: '**/*.html',
+        partials: mapHandlebarsPartials(metalsmith, 'layouts', 'partials'),
+        helpers: {
+          excerpt: require('../scripts/helpers/excerpt'),
+          moment: require('../scripts/helpers/moment')
+        }
+      }))
+      .destination(path.join(dir, 'dist'))
 
-        metalsmith.build(function (err) {
-            if (err) { throw err; }
-            done && done();
-        });
+  metalsmith.build(function (err) {
+    if (err) {
+      throw err
+    }
+    done && done()
+  })
 }
 
-
-function copyStatic(done) {
-    ncp(path.join(dir, 'static'), path.join(dir, 'dist', 'static'), function(err) {
-        if (err) {
-            throw err
-        }
-        done && done();
-    });
+function copyStatic (done) {
+  ncp(path.join(dir, 'static'), path.join(dir, 'dist', 'static'), function (err) {
+    if (err) {
+      throw err
+    }
+    done && done()
+  })
 }
 
 var buildAndCopy = function (done) {
-    build(function(){
-        copyStatic(function() { done && done(); console.log('build done!') })
-    });
-};
+  build(function () {
+    copyStatic(function () {
+      done && done()
+      console.log('build done!')
+    })
+  })
+}
 
-buildAndCopy();
+buildAndCopy()
 
-module.exports = buildAndCopy;
+module.exports = buildAndCopy
