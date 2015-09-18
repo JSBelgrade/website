816f228881d7169506c0472f65447c2873966e63
Goran Gajic <fero.null@gmail.com>
2015-09-18 16:22:43 +0200

    fix(helpers): convert 'html' to string because sometimes is buffer
    probably fixes #15

--- scripts/helpers/excerpt.js
+++ scripts/helpers/excerpt.js
@@ -8,7 +8,7 @@ module.exports = function(html, limit, options) {
     if (!options) {
         limit = 26;
     }
-
+    html = html.toString();
     // strip html tags
     html = html.replace(/<\/?[^>]+>/gi, '');
     html = html.replace(/(\r\n|\n|\r)+/gm, ' ');
